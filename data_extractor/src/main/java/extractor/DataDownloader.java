0206918d8f94761d03e8c4fcb65c5e416b62ad2e
Pranav Acharya <apranav.acharya@gmail.com>
2022-01-29 16:06:23 -0500

    added all file download method

--- data_extractor/src/main/java/extractor/DataDownloader.java
+++ data_extractor/src/main/java/extractor/DataDownloader.java
@@ -1,16 +1,12 @@
 package extractor;
 
-
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -22,68 +18,64 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class DataDownloader {
-	
+
 	private static final String bucketName = "noaa-nexrad-level2";
 	private static final Regions bucketRegion = Regions.US_EAST_1;
 	private static final String tempFolder = "./temp";
-	
-	private static void downloadNexradData() {
-//		String station, String yyyy, String mm, String dd
 
-        try {
-            AmazonS3 s3Client = new AmazonS3Client();
-
-            System.out.println("Listing objects");
-
-            // maxKeys is set to 2 to demonstrate the use of
-            // ListObjectsV2Result.getNextContinuationToken()
-            ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withPrefix("2022/01/26/FOP1/").withMaxKeys(2);
-            ListObjectsV2Result result;
-
-            do {
-                result = s3Client.listObjectsV2(req);
-
-                for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
-                    System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
-                }
-                // If there are more than maxKeys keys in the bucket, get a continuation token
-                // and list the next objects.
-                String token = result.getNextContinuationToken();
-                System.out.println("Next Continuation Token: " + token);
-                req.setContinuationToken(token);
-            } while (result.isTruncated());
-        } catch (AmazonServiceException e) {
-            // The call was transmitted successfully, but Amazon S3 couldn't process 
-            // it, so it returned an error response.
-            e.printStackTrace();
-        } catch (SdkClientException e) {
-            // Amazon S3 couldn't be contacted for a response, or the client
-            // couldn't parse the response from Amazon S3.
-            e.printStackTrace();
-        }
-    }
-	
-	private boolean downloadFromS3(String bucket_name, String key_name) throws Exception {
-		System.out.format("Downloading %s from S3 bucket %s...\n", key_name, bucket_name);
-		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(bucketRegion).build();
-	    S3Object o = s3.getObject(bucket_name, key_name);
-	    S3ObjectInputStream s3is = o.getObjectContent();
-	    Files.createDirectories(Paths.get(tempFolder));
-	    String[] fileNameToken = key_name.split("/");
-	    String shortName = fileNameToken[fileNameToken.length - 1];
-	    FileOutputStream fos = new FileOutputStream(new File(tempFolder + "/" + shortName));
-	    byte[] read_buf = new byte[1024];
-	    int read_len = 0;
-	    while ((read_len = s3is.read(read_buf)) > 0) {
-	        fos.write(read_buf, 0, read_len);
-	    }
-	    s3is.close();
-	    fos.close();
-		return true;
-	}
-	
-	public static void main(String args[]) throws Exception {
+	public static List<String> downloadNexradData(String stationID, String year, String month, String date, String start,
+			String end) {
+		List<String> downloaded_files = new ArrayList<String>();
 		DataDownloader dd = new DataDownloader();
-		System.out.println(dd.downloadFromS3(bucketName, "2022/01/26/FOP1/FOP120220126_000904_V06"));
+		List<String> fileList = dd.getFileListFromS3(stationID, date, month, year, start, end);
+		for (String file : fileList) {
+			try {
+				downloaded_files.add(dd.downloadFromS3(file));
+			} catch (Exception error) {
+				error.printStackTrace();
+				// ignore exception
+			}
+		}
+		return downloaded_files;
+	}
+
+	private String downloadFromS3(String key_name) throws Exception {
+		System.out.format("Downloading %s from S3 bucket %s...\n", key_name, bucketName);
+		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(bucketRegion).build();
+		S3Object o = s3.getObject(bucketName, key_name);
+		S3ObjectInputStream s3is = o.getObjectContent();
+		Files.createDirectories(Paths.get(tempFolder));
+		String[] fileNameToken = key_name.split("/");
+		String shortName = fileNameToken[fileNameToken.length - 1];
+		FileOutputStream fos = new FileOutputStream(new File(tempFolder + "/" + shortName));
+		byte[] read_buf = new byte[1024];
+		int read_len = 0;
+		while ((read_len = s3is.read(read_buf)) > 0) {
+			fos.write(read_buf, 0, read_len);
+		}
+		s3is.close();
+		fos.close();
+		return shortName;
+	}
+
+	private List<String> getFileListFromS3(String stationID, String dd, String mm, String yyyy, String start,
+			String end) {
+		List<String> fileList = new ArrayList<String>();
+		@SuppressWarnings("deprecation")
+		AmazonS3 s3Client = new AmazonS3Client();
+		String prefix = yyyy + "/" + mm + "/" + dd + "/" + stationID + "/";
+		ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withPrefix(prefix);
+		ListObjectsV2Result result;
+		result = s3Client.listObjectsV2(req);
+		for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+			System.out.printf(" - %s (size: %d)\n", objectSummary.getKey(), objectSummary.getSize());
+			String key = objectSummary.getKey();
+			String[] tokens = key.split("_");
+			String hhmmss = tokens[1];
+			if (hhmmss.compareTo(start) >= 0 && hhmmss.compareTo(end) <= 0) {
+				fileList.add(objectSummary.getKey());
+			}
+		}
+		return fileList;
 	}
 }
