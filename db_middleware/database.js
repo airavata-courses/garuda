5256eed3bb7159296c4cf90ea3204ef602896ddc
Pranav Acharya <apranav.acharya@gmail.com>
2022-02-03 05:27:15 -0500

    added databse connection error catch

--- db_middleware/database.js
+++ db_middleware/database.js
@@ -1,13 +1,17 @@
+const CONSTANTS = require("./constants");
+const mongoose = require("mongoose");
 
-const CONSTANTS = require('./constants')
-const mongoose = require('mongoose');
-mongoose.connect(CONSTANTS.CONST_MONGO_DB_SERVER_URL, {useNewUrlParser: true});
+mongoose
+  .connect(CONSTANTS.CONST_MONGO_DB_SERVER_URL, { useNewUrlParser: true })
+  .catch((err) => {console.log("error in connecting to database")});
+  
 const conn = mongoose.connection;
-conn.on('connected', function() {
-    console.log('database is connected successfully');
+conn.on("connected", function () {
+  console.log("database is connected successfully");
 });
-conn.on('disconnected',function(){
-    console.log('database is disconnected successfully');
-})
-conn.on('error', console.error.bind(console, 'connection error:'));
+conn.on("disconnected", function () {
+  console.log("database is disconnected successfully");
+});
+conn.on("error", console.error.bind(console, "connection error:"));
+
 module.exports = conn;
