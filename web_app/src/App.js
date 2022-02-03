3e81958039935816257dc8b832d10f3fb602712e
Pranav Acharya <apranav.acharya@gmail.com>
2022-02-02 20:16:54 -0500

    removed login button on home screen

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
-import Logout from './components/Logout';
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to GARUDA</h2>
         <Login />
-        <br />
-        {/* <Logout />
-        <br /> */}
     </div>
   );
 }
