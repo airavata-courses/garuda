9ce191766068dc08d6a37935e14234ade4c03fe0
rishi1596 <rishabh.jain53@gmail.com>
2022-04-03 19:41:04 -0400

    refresh button fix
    known issue: when clicked on browser refresh button it gives 404

--- web_app/src/components/Dashboard.js
+++ web_app/src/components/Dashboard.js
@@ -22,9 +22,8 @@ export default function Dashboard() {
   let station = [];
   let type = "nexrad"
 
-  // TODO: fix refresh button
   function refreshDashboard() {
-    navigateObj('/Dashboard')
+    navigateObj('/', {replace:true})
   }
 
   const sendDataToParent = (response) => {
@@ -109,7 +108,7 @@ export default function Dashboard() {
               document.getElementById("apiResponseMsg").innerHTML =
                 result.response_message;
               setTimeout(() => {
-                navigateObj('/Dashboard', { replace: true })
+                refreshDashboard()
               }, 2000)
             } else {
               // Error
